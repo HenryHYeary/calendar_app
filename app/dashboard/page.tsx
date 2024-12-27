b58095fcccfcb100a2f92d25f0e9955ce5457c66
HenryHYeary <HenryHYeary@github.com>
2024-12-27 11:16:45 -0600

    Add UI for adding an event type.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,15 +1,50 @@
-import { redirect } from "next/navigation"
-import { auth } from "../utils/auth"
 import requireUser from "../utils/hooks"
+import prisma from "../utils/db"
+import { notFound } from "next/navigation"
+import EmptyState from "../components/EmptyState"
+
+async function getData(userId: string) {
+  const data = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      userName: true,
+      eventType: {
+        select: {
+          id: true,
+          active: true,
+          title: true,
+          url: true,
+          duration: true
+        }
+      }
+    }
+  })
+
+  if (!data) {
+    return notFound()
+  }
+
+  return data
+}
 
 
 const DashboardPage = async () => {
   const session = await requireUser()
+  const data = await getData(session.user?.id as string)
 
   return (
-    <div>
-      <h1>Hello from the dashboard!</h1>
-    </div>
+    <>
+      {data.eventType.length === 0 ? (
+        <EmptyState title="You have no Event Types" description="You can create
+        your first Event Type by clicking the button below"
+        buttonText="Add event type"
+        href="/dashboard/new"/>
+      ) : (
+        <p>we have event types</p>
+      )}
+    </>
   )
 }
 
