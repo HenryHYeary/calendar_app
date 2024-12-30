2cbd50e71b37226af5e4419727ed9e9661259526
HenryHYeary <HenryHYeary@github.com>
2024-12-30 10:43:22 -0600

    Add UI for event type CRUD operations

--- app/utils/zodSchemas.ts
+++ app/utils/zodSchemas.ts
@@ -1,4 +1,5 @@
 import { conformZodMessage } from "@conform-to/zod"
+import { title } from "process"
 import { z } from "zod"
 
 export const onboardingSchema = z.object({
@@ -51,4 +52,12 @@ export function onboardingSchemaValidation(options?: {
 export const settingsSchema = z.object({
   fullName: z.string().min(3).max(150),
   profileImage: z.string(),
+})
+
+export const eventTypeSchema = z.object({
+  title: z.string().min(3).max(150),
+  duration: z.number().min(15).max(60),
+  url: z.string().min(3).max(150),
+  description: z.string().min(3).max(300),
+  videoCallSoftware: z.string().min(3),
 })
---
+++ components/ui/ButtonGroup.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { cn } from "@/lib/utils"
+import { Children, cloneElement, ReactElement } from "react"
+import { ButtonProps } from "./button"
+
+interface ButtonGroupProps {
+  className?: string,
+  children: ReactElement<ButtonProps>[],
+}
+
+const ButtonGroup = ({className, children}: ButtonGroupProps) => {
+  const totalButtons = Children.count(children)
+  return (
+    <div className={cn("flex w-full", className )}>
+      {children.map((child, index) => {
+        const isFirstItem = index === 0
+        const isLastItem = index === totalButtons - 1
+
+        return cloneElement(child, {
+          className: cn(
+            {
+              "rounded-l-none": !isFirstItem,
+              "rounded-r-none": !isLastItem,
+              "border-l-0": !isFirstItem
+            }, 
+            child.props.className,
+          ),
+          key: index
+        })
+      })}
+    </div>
+  )
+}
+
+export default ButtonGroup
