f89f4fcbb5648c418cd88c783e3d837d5b6392c3
Henry Yeary <henry-yeary@Henrys-Mac-mini.local>
2025-01-10 16:41:16 -0600

    Add UI for landing page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
 import { redirect } from "next/navigation";
 import { Navbar } from "./components/Navbar";
 import { auth } from "./utils/auth";
+import Hero from "./components/Hero";
+import Logos from "./components/Logos";
+import Features from "./components/Features";
+import CTA from "./components/Cta";
 
 export default async function Home() {
   const session = await auth()
@@ -11,6 +15,10 @@ export default async function Home() {
   return (
     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
       <Navbar />
+      <Hero />
+      <Logos />
+      <Features />
+      <CTA />
     </div>
   );
 }
